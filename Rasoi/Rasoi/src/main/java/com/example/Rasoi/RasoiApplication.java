a9e13edf7b981e6aeb4377fc0842c91b9ddea7a5
Nishant Singh <nishasingh@BLR.ALLEGISINDIA.COM>
2023-12-01 12:01:33 +0530

    just for adding

---
+++ Rasoi/Rasoi/src/main/java/com/example/Rasoi/RasoiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rasoi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasoiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasoiApplication.class, args);
+	}
+
+}
---
+++ Rasoi/Rasoi/src/test/java/com/example/Rasoi/RasoiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rasoi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasoiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
